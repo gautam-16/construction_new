bccf9b5f6b26092146bcf66766809bf84585e781
gautam-16 <atre.gautam@cubexo.io>
2022-10-10 15:56:27 +0530

    changes in api

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router=express.Router()
-const {createUser, readUser,readOneUser,updateUser,deleteUser}=require('../controller/user.Controller')
+const {createUser, readUser,readOneUser,updateUser,deleteUser}=require('../controller/user.controller')
 router.route('/userCreate').post(createUser)
 router.route('/readUser').get(readUser)
 router.route('/readOneUser').get(readOneUser)
