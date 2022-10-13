ee348af6dab106f43181c9f28c3f648a19e31ac9
gautam-16 <atre.gautam@cubexo.io>
2022-10-13 17:05:37 +0530

    authentication function changes

--- backend/routes/fix_table.routes.js
+++ backend/routes/fix_table.routes.js
@@ -2,6 +2,7 @@ const {roleCreate,statusCreate} = require("../controller/fixed_table.controller"
 const {isAuthenticated}=require('../middlewares/user.authentication')
 const express  =  require('express');
 const router = express.Router();
+
 router.route('/createRole').post(roleCreate);
 router.route('/createStatus').post(statusCreate);
 
