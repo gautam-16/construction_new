7bcfadb42a7861e94d992da6c2730cdd12e4e885
satyam81030 <solankisatyam3105@gmail.com>
2022-10-17 19:07:16 +0530

    updateProject and DeleteProject

--- backend/controller/project.controller.js
+++ backend/controller/project.controller.js
@@ -1,6 +1,7 @@
-const Project=require('../models/project.model')
+const Project = require('../models/project.model')
 const User = require('../models/user.model')
-const EmployeeonProject=require('../models/employees.on.project.model')
+const changelogProject = require('../models/changelog.project')
+const EmployeeonProject = require('../models/employees.on.project.model')
 const sequelize = require('../server')
 const { Sequelize } = require('sequelize')
 const { emptyQuery } = require('pg-protocol/dist/messages')
@@ -33,27 +34,27 @@ exports.createProject=async(req,res)=>{
         return res.status(500).json({message:error.message})
     }
 }
-exports.AssignUser=async(req,res)=>{
-    try {
-        let project=await Project.findOne({ where:{projectname:req.body.projectname}});
-        let user=await User.findOne({where:{userid:req.body.userid}})
-        // console.log(project)
-        if(req.body.role=='principalarchitect'){
-        console.log(req.user.name)
-        Project.update({principalarchitect:user.userid},{where:{projectname:req.body.projectname}})
-        EmployeeonProject.create({userid:user.id,userdesignation:user.designation,assignedby:req.user.name,user,projectname:project.projectname})
-        return res.status(201).json({message:`${req.body.role} role assigned successfully`})
-        }
-        else{
-            console.log(req.user.name)
-            Project.update({projectmanager:user.userid},{where:{projectname:req.body.projectname}})
-            EmployeeonProject.create({userid:user.id,userdesignation:user.designation,assignedby:req.user.name,user,projectname:project.projectname})
-            return res.status(201).json({message:`${req.body.role} role assigned successfully`})
-        }
-    } catch (error) {
-        return res.status(500).json({message:error.message})
-        
+exports.AssignUser = async (req, res) => {
+  try {
+    let project = await Project.findOne({ where: { projectname: req.body.projectname } });
+    let user = await User.findOne({ where: { userid: req.body.userid } })
+    // console.log(project)
+    if (req.body.role == 'principalarchitect') {
+      console.log(req.user.name)
+      Project.update({ principalarchitect: user.userid }, { where: { projectname: req.body.projectname } })
+      EmployeeonProject.create({ userid: user.id, userdesignation: user.designation, assignedby: req.user.name, user, projectname: project.projectname })
+      return res.status(201).json({ message: `${req.body.role} role assigned successfully` })
     }
+    else {
+      console.log(req.user.name)
+      Project.update({ projectmanager: user.userid }, { where: { projectname: req.body.projectname } })
+      EmployeeonProject.create({ userid: user.id, userdesignation: user.designation, assignedby: req.user.name, user, projectname: project.projectname })
+      return res.status(201).json({ message: `${req.body.role} role assigned successfully` })
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+
+  }
 }
 exports.getallProjects=async(req,res)=>{
     try{
@@ -77,3 +78,57 @@ exports.getallProjects=async(req,res)=>{
           res.status(500).json({ message: error.message })
         }
       }
+
+exports.updateOneProject = async (req, res) => {
+  try {
+    const project = await Project.findByPk(req.params._id);
+    if (req.user.level <= 1) {
+      if (project.isactive==false) {
+         return res.status(500).json("Project is not longer available")
+      }else{
+      await changelogProject.create({
+        projectname: project.projectname,
+        projectaddress: project.projectaddress,
+        owner: project.owner,
+        ownercontact: project.ownercontact,
+        city: project.city,
+        owneremail: project.owneremail,
+        startdate: project.startdate,
+        enddate: project.enddate
+      }).then(() => {
+        Project.update(
+          req.body,
+          { where: { id: req.params._id } }
+        ).then(() => { return res.status(200).json({ message: "Successfuly Updated" }) }).catch((error) => { return res.status(500).json({ error: error.message }) })
+
+      }).catch((error) => { return res.status(500).json({ message: error.message }) })
+    }
+    }
+    else {
+      return res.status(404).json("You don't have access")
+    }
+
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+}
+
+exports.deleteOneProject = async(req,res)=>{
+  try {
+    const project = await Project.findByPk(req.params._id);
+    if (req.user.level <= 1) {
+      if (project.isactive==false) {
+        return res.status(500).json("Project is already Deleted")
+     }else{
+      Project.update(
+        { isactive: false },
+        { where: { id: req.params._id } }
+      ).then(() => { return res.status(200).json({ message: "Project Successfuly Deleted" }) }).catch((error) => { return res.status(500).json(error) })
+      }
+    }
+    else {
+      return res.status(404).json("You dont have access for it")
+    }
+  } catch (error) {
+  }
+}
--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const router=express.Router()
-const {createProject, AssignUser, getallProjects}=require('../controller/project.controller')
+const {createProject, AssignUser, getallProjects,updateOneProject,deleteOneProject}=require('../controller/project.controller')
 const{isAuthenticated}=require('../middlewares/user.authentication')
 router.route('/createProject').post(isAuthenticated,createProject)
 router.route('/assignUser').post(isAuthenticated,AssignUser)
 router.route('/allproject').get(isAuthenticated,getallProjects)
-
+router.route('/updateOneProject/:_id').put(isAuthenticated,updateOneProject)
+router.route('/deleteOneProject/:_id').delete(isAuthenticated,deleteOneProject)
 
 module.exports=router;
