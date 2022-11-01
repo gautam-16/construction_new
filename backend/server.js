c67b204aaddfae64b80293b72d889380e2f9d5e6
gautam-16 <atre.gautam@cubexo.io>
2022-11-01 18:52:19 +0530

    Pending

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,11 @@ const dbConfig = require('./config/.env')
 const Sequelize = require("sequelize");
 
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+const sequelize = new Sequelize(
+  dbConfig.database, dbConfig.username, dbConfig.password, {
   host: dbConfig.host,
   dialect: dbConfig.dialect,
   operatorsAliases: 0,
-  // pool: {
-  //   max: dbConfig.pool.max,
-  //   min: dbConfig.pool.min,
-  //   acquire: dbConfig.pool.acquire,
-  //   idle: dbConfig.pool.idle
-  // },
   dialectOptions: {
     ssl: {
         require: true,
