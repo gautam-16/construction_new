1ddb23239aa150c8d330397c66b6c9729953f30c
satyam81030 <solankisatyam3105@gmail.com>
2022-11-22 13:47:26 +0530

    update and delete on task

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,8 +1,10 @@
 const express=require('express')
 const router=express.Router()
 const {isAuthenticated}=require('../middlewares/user.authentication')
-const {createTask,getAlltasks}=require('../controller/task.controller')
+const {createTask,getAlltasks, updateTask, deleteTask}=require('../controller/task.controller')
 
 router.route('/createTask/:phaseid').post(isAuthenticated,createTask)
 router.route('/getAllTasks/:phaseid').get(isAuthenticated,getAlltasks)
+router.route('/updateTask/:phaseid').put(isAuthenticated,updateTask)
+router.route('/deleteTask/:phaseid').delete(isAuthenticated,deleteTask)
 module.exports=router;
